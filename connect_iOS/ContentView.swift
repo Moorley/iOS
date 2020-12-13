13d69610581609ce9f83860a56e6a73025e53bea
Akutagawa-Rikiya <s003001a0831@gmail.com>
2020-12-13 17:59:24 +0900

    Initial Commit

---
+++ connect_iOS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  connect_iOS
+//
+//  Created by 芥川力也 on 2020/12/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ connect_iOS/connect_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  connect_iOSApp.swift
+//  connect_iOS
+//
+//  Created by 芥川力也 on 2020/12/13.
+//
+
+import SwiftUI
+
+@main
+struct connect_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
