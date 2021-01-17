c41b9bbf30daa8505e17974228311e377a183fbe
Akutagawa-Rikiya <s003001a0831@gmail.com>
2021-01-17 22:34:08 +0900

    友達追加機能のポップアップ更新 ホーム画面ナビゲーション一部修正

--- connect_iOS/View/AddFriend.swift
+++ connect_iOS/View/AddFriend.swift
@@ -9,15 +9,25 @@ import SwiftUI
 
 struct AddFriend: View {
     @State var name : String = ""
+    @State private var isShowSelect = false
     var body: some View {
         VStack{
             Text("名前を入力してください。")
             TextField("名前",text: $name)
             .textFieldStyle(RoundedBorderTextFieldStyle())
             Button(action:{
+                self.isShowSelect = true
                 
             }){
                 Text("add")
+            }
+            .alert(isPresented: $isShowSelect) {
+                        Alert(title: Text("確認"),
+                              message: Text("\(name)を追加します。"),
+                              primaryButton: .default(Text("いいえ")),
+                              secondaryButton: .default(Text("はい")) {
+                                // 「はい」を選択した場合の処理
+                              })
             }
         }
     }
--- connect_iOS/View/HomeView.swift
+++ connect_iOS/View/HomeView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct HomeView: View {
     
     var body: some View {
-        NavigationView{
+        
         VStack{
             Text("私の友達")
             List{
@@ -27,7 +27,7 @@ struct HomeView: View {
             Spacer()
             MenuRowView()
         }
-    }
+    
     }
 
 }
